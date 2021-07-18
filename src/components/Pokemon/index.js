87ae32331dd90ccc81c0907020749114bce00648
Lee Farnell <leefarnell@mac.com>
2021-07-18 16:11:44 +0100

    Updated index file

--- src/components/Pokemon/index.js
+++ src/components/Pokemon/index.js
@@ -1,6 +1,5 @@
 const Pokemon = (props) => {
-  // console.log(props.cardData.data);
-
+  // -- PokeDex Data --
   const newDexEntry = [];
 
   const dexEntry = props.dexData.flavor_text_entries;
@@ -13,8 +12,11 @@ const Pokemon = (props) => {
 
   dexEntry.map(dexCallback);
 
+  // -- Height/Weight Data --
   const height = props.data.height / 10;
   const weight = props.data.weight / 10;
+
+  // -- Ability Data --
 
   const abilityCallback = (data) => {
     return <li key={data.ability.name}> {data.ability.name} </li>;
@@ -22,17 +24,21 @@ const Pokemon = (props) => {
 
   const abilities = props.data.abilities.map(abilityCallback);
 
+  // -- Type Data --
   const typeCallback = (data) => {
     return <li key={data.type.name}> {data.type.name} </li>;
   };
   const types = props.data.types.map(typeCallback);
 
+  // -- Pokemon Card Data --
   const cardCallback = (data) => {
     if (data) {
       return (
         <div className="col-3 flex-wrap p-2">
           <a href={data.images.large} target="_blank" rel="noreferrer">
             <img
+              id={data.id}
+              key={data.id}
               src={data.images.large}
               className="rounded mx-auto d-block image"
               alt={data.name}
@@ -40,14 +46,18 @@ const Pokemon = (props) => {
           </a>
         </div>
       );
-    } else {
-      return "No known cards.";
+    } else if (data === [""]) {
+      return (
+        <div className="col-3 flex-wrap p-2">
+          <h3> No known cards.</h3>
+        </div>
+      );
     }
   };
 
   const cards = props.cardData.data.map(cardCallback);
-  console.log(cards);
 
+  // -- Render Card --
   return (
     <div className="card">
       <div className="row justify-content-center text-center d-flex m-0">
